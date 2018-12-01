5d1dcbfe3c4b94cfdbf48f38ad48d08582119d5b
sanidhya70 <ksanidhya007@gmail.com>
2018-11-30 19:52:33 -0500

    Added methods to the Controller.

--- src/main/java/com/example/demo/InventoryController.java
+++ src/main/java/com/example/demo/InventoryController.java
@@ -1,7 +1,8 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -10,5 +11,30 @@ public class InventoryController {
 	    public String index() {
 	        return "Congratulations from InventoryController.java";
 	    }
+	 
+	 @GetMapping("/items")
+	 public String allproducts() 
+	 {
+		return null;
+		 
+	 }
+	 
+	 @GetMapping("/items/{id}")
+	 public String selectedproduct(@PathVariable String id){
+		 return null;
+	 }
+	 
+	 @PostMapping("/items/new")
+	    public InventoryController create(@RequestBody Map<String, String> body){
+	        int product_id = Integer.parseInt(body.get("product_id"));
+	        String product_name = body.get("product_name");
+	        String product_category = body.get("product_category");
+	        String total_item_remaining = body.get("total_item_remaining");
+	        String product_location = body.get("product_location");
+	        return null;
+	 }
+	 
+	 
+
 
 }
